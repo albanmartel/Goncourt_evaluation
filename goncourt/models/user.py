2c715ab0d7df515f438342987231ac1e673f3095
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-17 13:47:16 +0200

    UPDATE: Modificiation des commentaires de la classe User

--- goncourt/models/user.py
+++ goncourt/models/user.py
@@ -11,13 +11,14 @@ from .person import Person
 class User(Person):
 
     """
-    L'author est un auteur qui a une biographie
-    Plutôt tiré par les cheveux comme conception
+    :param user_street: le nom de la rue de l'utilisateur
+    :param user_city: le nom de la ville de l'utilisateur
+    :param user_postal_code: le code postal de l'adresse de l'utilisateur
     :param user_email le courriel d'un utilisateur
     :param user_phone le numéro de téléphone
 
     """
-    user_street: str = 0  # nb d'étudiants créés
+    user_street: str =  "unknown"
     user_city: str = "unknown"
     user_postal_code = "unknown"
     user_email: str = "unknown"
