61401de469877d963012bebfb17daca8b733b6c3
Alban MARTEL <albanmartel@users.noreply.github.com>
2026-09-20 21:01:07 +0000

    Update goncourt.py modification de la méthode 'get_author_by_id()

--- goncourt/business/goncourt.py
+++ goncourt/business/goncourt.py
@@ -14,16 +14,27 @@ from models.author import Author
 
 @dataclass
 class Goncourt:
-    """Couche métier de l'application de la selection du prix Goncourt,
+    """
+    Couche métier de l'application de la selection du prix Goncourt,
     reprenant les cas d'utilisation et les spécifications fonctionnelles :
     - authors : liste des auteurs enregistrés
-    - teachers : liste des enseignants
-    - students : liste des élèves"""
+    - members : liste des membres du jury
 
+    """
     Authors: list[Author] = field(default_factory=list, init=False)
 
     @staticmethod
-    def get_author_by_id(id_author: int) -> Optional[Author]:
+    def get_author_by_id(id_author: int) -> Author | None:
+                """
+        Méthode pour obtenir un auteur depuis la classe business
+        Goncourt
+
+        Args:
+            id_author: l'id de l'auteur recherché
+
+        Returns: un objet DaoAuthor
+
+        """
         author_dao: AuthorDao = AuthorDao()
         return author_dao.read(id_author)
 
