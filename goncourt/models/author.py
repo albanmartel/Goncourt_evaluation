df613e9d3f5e8875b07d26bc47f21f6032e521df
Alban MARTEL <albanmartel@users.noreply.github.com>
2026-09-20 13:52:07 +0000

    Update author.py ajout d'une méthode pour pouvoir itérer avec l'objet Author

--- goncourt/models/author.py
+++ goncourt/models/author.py
@@ -11,13 +11,28 @@ from .user import User
 class Author(User):
 
     """
-    L'autheur est un utilisateur qui a une biographie
+    L'auteur est un utilisateur qui a une biographie
     et qui écrit un  ou des livres.
     Plutôt tiré par les cheveux comme conception
     """
-    biography: str = "unknown"
-    book_title: str = "unknown"
-    book_summary: str = "unknown"
+    biography: str
+    id_author: int
+
+    def items(self):
+        """
+        méthode pour pouvoir itérer comme avec un dictionnaire
+        """
+        yield "first_name", self.first_name
+        yield "last_name", self.last_name
+        yield "book_title", self.book_title
+        yield "book_summary", self.book_summary
+        yield "biography", self.biography
+        yield 'id_author', self.id_author
+        yield 'user_street', self.user_street
+        yield 'user_postal_code', self.user_postal_code
+        yield 'user_city', self.user_city
+        yield 'user_email', self.user_email
+        yield 'user_phone', self.user_phone
 
     def toString(self):
         return f"{self.author_biography}"
