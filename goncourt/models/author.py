889be86d5be496a4b23b40f0b1b8fb92051c38f8
Alban Martel <alban.f.j.martel@laposte.net>
2026-09-20 17:53:25 +0200

    UPDATE: 'goncourt/models/author.py' Ajouter une méthode pour ajouter une biographie à un auteur

--- goncourt/models/author.py
+++ goncourt/models/author.py
@@ -1,20 +1,20 @@
-# -*- coding: utf-8 -*-
-
 """
 Classe Author, fille de la classe User
 """
 
-from dataclasses import dataclass, asdict
+from dataclasses import asdict, dataclass
+
 from .user import User
+
 
 @dataclass
 class Author(User):
-
     """
     L'auteur est un utilisateur qui a une biographie
-    et qui écrit un  ou des livres.
+    et qui écrit un ou des livres.
     Plutôt tiré par les cheveux comme conception
     """
+
     biography: str = "unknown"
     id_author: int = 0
 
@@ -27,11 +27,22 @@ class Author(User):
         """
         return asdict(self).items()
 
-     def __str__(self) -> str:
+    def add_biography(self, string_biography: str) -> None:
+        """
+
+        Args:
+            string_biography: la chaîne de caractère qui remplace
+             la précédente chaîne de la biographie de l'auteur
+
+        Returns: None
+
+        """
+        self.biography = string_biography
+
+    def __str__(self) -> str:
         """
         Méthode qui permet les données de l'instance de la classe Auteur
 
         :return: une chaîne de caractère possédant toutes les données de la classe
         """
         return f"{self.first_name} {self.last_name} {self.book_title} {self.book_summary} {self.biography} {self.user_street} {self.user_postal_code} {self.user_city} {self.user_email} {self.user_phone}"
-
